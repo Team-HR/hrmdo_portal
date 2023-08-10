00fb1721b0389950763bfba456ebce248be3b97c
fjavalencia <franzval123@gmail.com>
2023-08-10 15:46:17 +0800

    Init upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <h1>WELCOME TO HRMDO PORTAL</h1>
+      <br>
+      <a href="/ihris" style="margin: 15px; padding: 15px;">Go to IHRIS</a>
+
+      <a href="/pcr" style="margin: 15px; padding: 15px;">Go to PCR-Online</a>
+      <!-- <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
